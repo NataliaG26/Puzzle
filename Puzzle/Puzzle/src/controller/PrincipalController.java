4df23fdfa7ba86af1b1d3eb3a177c779eaaa4214
Duvan Cuero <duvancuero@hotmail.com>
2019-05-25 13:35:41 -0500

    R

--- Puzzle/Puzzle/src/controller/PrincipalController.java
+++ Puzzle/Puzzle/src/controller/PrincipalController.java
@@ -1,17 +1,30 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class PrincipalController implements Initializable{
 	
 	@FXML
     void bestSocres(ActionEvent event) {
-		
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("../userInterface/BestScores.fxml"));
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root));
+		stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 
     @FXML
